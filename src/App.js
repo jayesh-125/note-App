f480423504138a0cc17a3171949d6675bbe43943
jayesh-125 <tankjayesh125@gmail.com>
2023-05-17 13:46:05 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,59 +1,9 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React from "react";
 function App() {
-  const [first, setfirst] = useState([]);
-  const [addMessage, setAddMessage] = useState("");
-  const [send, setsend] = useState("Aonymias");
-  const addMes = () => {
-    if (addMessage.trim() !== "") {
-      const message = { send: send, content: addMessage };
-      setfirst([...first, message]);
-      setAddMessage("");
-    }
-  };
-  useEffect(() => {
-    const chatScreen = document.getElementById("chatSc");
-    chatScreen.scrollTop = chatScreen.scrollHeight;
-  }, [first]);
-
+ 
   return (
     <>
-      <div className="main">
-        <div className="message" id="chatSc">
-          {first.map((message, index) => (
-            <div className="message" key={index}>
-              <span>
-                {message.send}: {message.content}{" "}
-              </span>
-              <i className="fa-solid fa-check"></i>
-            </div>
-          ))}
-          <div className="mas">
-            <button className="mic">
-              <i className="fa-solid fa-microphone"></i>
-            </button>
-            <form action="">
-              <input
-                type="text"
-                className="input_message"
-                placeholder="Enter message.."
-                value={addMessage}
-                onChange={(e) => setAddMessage(e.target.value)}
-              />
-              <input
-                type="text"
-                className="input_message"
-                placeholder="Enter message.."
-                value={send}
-                onChange={(e) => setsend(e.target.value)}
-              />
-              <button className="btn-md btn" onClick={addMes}>
-                send
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
     </>
   );
 }
--- src/Component1.js
+++ src/Component1.js
@@ -7,17 +7,18 @@ function Component1() {
     const newText = addText.toUpperCase();
     setText(newText);
   };
-
+  
   const changeText = (e) => {
     setText(e.target.value);
   };
 
   return (
     <div className="container">
-      <input type="text" value={addText} onChange={changeText} name="" id="" />
-      <span>{text}</span>
-      <button className="btn" onClick={text}>
-        change
+      <input type="text" value={addText} className="input_message m-2" onChange={changeText} name="" id="" />
+      <br />
+      <span>{addText.split(" ").length} charecters is {addText.length}</span>
+      <button className="m-3" onClick={text}>
+        changetoUPPERCASE
       </button>
     </div>
   );
