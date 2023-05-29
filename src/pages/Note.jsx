f682cbe1ee78cc97bd79cbd8744f43ce12823de8
jayesh-125 <tankjayesh125@gmail.com>
2023-05-29 12:40:20 +0530

    note App

--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 function Note() {
   return (
     <div>
-      Hiii
+      <section>
+        <header className='header'>
+          <h2 className='h2'>NOTE</h2>
+          <div>
+            <input type="text" className='inp' autoFocus placeholder='keyword...' />
+            <button className='btn'>search</button>
+          </div>
+        </header>
+      </section>
+      <section>
+        <div className="container">
+          
+        </div>
+      </section>
     </div>
   )
 }
